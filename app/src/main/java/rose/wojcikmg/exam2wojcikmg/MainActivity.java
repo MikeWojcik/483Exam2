cb58559f7ae4d69b37ef5ef4e29117011a8cff4e
MikeWojcik <32883763+MikeWojcik@users.noreply.github.com>
2018-03-31 16:41:25 -0400

    test2

---
+++ app/src/main/java/rose/wojcikmg/exam2wojcikmg/MainActivity.java
@@ -0,0 +1,13 @@
+package rose.wojcikmg.exam2wojcikmg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
