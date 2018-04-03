a7c02b1494490c3525738b050c9f1845ea058fd2
MikeWojcik <32883763+MikeWojcik@users.noreply.github.com>
2018-04-02 22:53:29 -0400

    Commit

--- app/src/main/java/rose/wojcikmg/exam2wojcikmg/MainActivity.java
+++ app/src/main/java/rose/wojcikmg/exam2wojcikmg/MainActivity.java
@@ -1,10 +1,9 @@
 package rose.wojcikmg.exam2wojcikmg;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
         ItemTouchHelper.Callback callback= new SwipeItemTouchCallback(mStateAdaptor);
         ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
         touchHelper.attachToRecyclerView(view);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        Toolbar top= findViewById(R.id.toolbar);
-       // top.se
+
 
 
     }
